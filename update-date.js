4772b7bca617adf22a0f0c2e40d9d2401a54a4d9
iGaliano <84567989+galetaire@users.noreply.github.com>
2026-08-15 12:11:34 +0200

    Update update-date.js

--- update-date.js
+++ update-date.js
@@ -1,5 +1,5 @@
 // update-date.js
-const LAST_UPDATE = "13/04/2026";
+const LAST_UPDATE = "15/08/2026";
 
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll(".last-update").forEach(el => {
